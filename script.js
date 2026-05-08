8cc1c7f068968c0bd3a88b7ef075f0abd647977d
NVfilm <dadadesigner19@gmail.com>
2026-05-08 11:47:53 +0530

    Implement city search and redirection functionality
    Added a function to search for cities and redirect.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("OfficeWings Loaded");
+function searchCity() {
+
+    let city = document.getElementById("cityInput").value.toLowerCase();
+
+    if(city === "delhi"){
+        window.location.href = "#delhi";
+    }
+
+    else if(city === "mumbai"){
+        window.location.href = "#mumbai";
+    }
+
+    else if(city === "bangalore"){
+        window.location.href = "#bangalore";
+    }
+
+    else if(city === "gurgaon"){
+        window.location.href = "#gurgaon";
+    }
+
+    else{
+        alert("City not found");
+    }
+
+}
